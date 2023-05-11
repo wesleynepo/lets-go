890ba8737439d702a28b46c91ba6e259a135ad35
wesleynepo <wesleynepomuceno1@gmail.com>
2023-05-11 20:46:39 -0300

    first

---
+++ cmd/web/templates.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"html/template"
+	"path/filepath"
+	"time"
+
+	"snippetbox.wesleynepo.com/internal/models"
+)
+
+func humanDate(t time.Time) string {
+    return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+    "humanDate": humanDate,
+}
+
+type templateData struct {
+    CurrentYear int
+    Snippet *models.Snippet
+    Snippets []*models.Snippet
+    Form any
+    Flash string
+    IsAuthenticated bool
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+    cache := map[string]*template.Template{}
+
+    pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+    if err != nil {
+        return nil, err
+    }
+
+    for _, page := range pages {
+        name := filepath.Base(page)
+
+        ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+        if err != nil {
+            return nil, err
+        }
+
+        ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+        if err != nil {
+            return nil, err
+        }
+
+        ts, err = ts.ParseFiles(page)
+        if err != nil {
+            return nil, err
+        }
+
+        cache[name] = ts
+    }
+
+    return cache, nil
+}
+
