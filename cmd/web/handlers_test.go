9b62cec49bc36beb42a40820a14810a988da5e2e
wesleynepo <wesleynepomuceno1@gmail.com>
2023-05-13 20:35:33 -0300

    feat: e2e test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,38 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippetbox.wesleynepo.com/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-    rr := httptest.NewRecorder()
+    app := newTestApplication(t)
+    
+    ts := newTestServer(t, app.routes())
+    defer ts.Close()
 
-    r, err := http.NewRequest(http.MethodGet, "/", nil)
+    code, _, body := ts.get(t, "/ping")
 
-    if err != nil {
-        t.Fatal(err)
-    }
-
-    ping(rr, r)
-
-    rs := rr.Result()
-
-    assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-    defer rs.Body.Close()
-
-    body, err := io.ReadAll(rs.Body)
-    if err != nil {
-        t.Fatal(err)
-    }
-
-    bytes.TrimSpace(body)
-
-    assert.Equal(t, string(body), "OK")
+    assert.Equal(t, code, http.StatusOK)
+    assert.Equal(t, body, "OK")
 }
